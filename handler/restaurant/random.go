ddb1ed05dbe0d012e4381c8c94d9b5f237b1e720
Bowser1704 <yuqlang@163.com>
2020-02-06 11:49:51 +0800

    <fix> fix readme

--- handler/restaurant/random.go
+++ handler/restaurant/random.go
@@ -9,7 +9,7 @@ import (
 //@Description 美食首页的推荐窗口
 //@Accept json
 //@Produce json
-//@Param canteen_name body string true "食堂名字"
+//@Param canteen_name body string true "食堂名字, 例如东一，学子"
 //@Success 200 {object} service.RandomRestaurant
 //@Router /restaurant/random [get]
 func Random(c *gin.Context) {
